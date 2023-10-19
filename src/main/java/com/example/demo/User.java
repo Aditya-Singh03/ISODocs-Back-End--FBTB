c3d707c95bade92af00b657b1cee1c9e910d317e
QDVO <quanvan18032003@gmail.com>
2023-10-18 20:47:58 -0400

    Add basic login call

---
+++ src/main/java/com/example/demo/User.java
@@ -0,0 +1,44 @@
+package com.example.demo;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.IdClass;
+import jakarta.persistence.Table;
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Table(name = "users_login")
+public class User {
+    @Id
+    @Column (name = "user_name")
+    private String name;
+
+    @Column (name = "hash_pass")
+    private String password;
+
+    private User() {}
+
+    public User(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ src/main/java/com/example/demo/UserController.java
@@ -0,0 +1,64 @@
+package com.example.demo;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.transaction.Transactional;
+
+@RestController
+public class UserController{
+    @Autowired
+    private final UserRepository userRepository;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping("/users/all")
+    public Iterable<User> findAllUsers() {
+        return this.userRepository.findAll();
+    }
+
+    @GetMapping("/users/{name}")
+    public User findByName(@PathVariable String name) {
+        System.out.println("SEARCHING");
+        return this.userRepository.findByName(name);
+    }
+
+    @GetMapping("/users/{name}/{password}")
+    public User findByNameAndPassword(@PathVariable String name, @PathVariable String password) {
+        return this.userRepository.findByNameAndPassword(name, password);
+    }
+
+    @GetMapping("/users/check/{name}/{password}")
+    public boolean existsByNameAndPassword(@PathVariable String name, @PathVariable String password) {
+        if(this.userRepository.existsByNameAndPassword(name, password)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @PostMapping("/users/add")
+    public User addUser(@RequestBody User newUser) {
+        String name = newUser.getName();
+        String password = newUser.getPassword();
+        User user = new User(name, password);
+        return this.userRepository.save(user);
+    }
+
+    @Transactional
+    @RequestMapping(value="/users/delete/{name}/{password}", method={RequestMethod.DELETE, RequestMethod.GET})
+    public void deleteUser(@PathVariable String name, @PathVariable String password) {
+        if(this.userRepository.existsByNameAndPassword(name, password)) {
+            this.userRepository.deleteByNameAndPassword(name, password);
+        }
+    }
+}
