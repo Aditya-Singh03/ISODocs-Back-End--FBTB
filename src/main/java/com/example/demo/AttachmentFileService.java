771772b80bab5110a0445c5415c6c501ff1ed952
Lorenzo Inglese <113322852+linglese@users.noreply.github.com>
2023-11-25 18:55:06 -0500

    Working Set Parameters

--- src/main/java/com/example/demo/AttachmentFileService.java
+++ src/main/java/com/example/demo/AttachmentFileService.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import java.util.List;
 
+import org.antlr.v4.runtime.atn.SemanticContext.OR;
 import org.springframework.stereotype.Service;
 
 
@@ -10,9 +11,9 @@ import jakarta.persistence.EntityManager;
 
 @Service
 public class AttachmentFileService {
-    public static List<AttachPropPrimaryKey> queryForDocuments(Long attachment_id, String file_name, String project_name, String customer_name, Long commitment_period_id, String resource_name) {
+    public static List<Object> queryForDocuments(Long attachment_id, String file_name, String project_name, String customer_name, Long commitment_period_id, String resource_name) {
         EntityManager em = EntityService.getEntityManagerFactory();
-        List<AttachPropPrimaryKey> var = em.createQuery("SELECT a.proposalId, a.attachmentId, file.name, prop.projectId, prop.projectType, prop.customerId, " +
+        List<Object> var = em.createQuery("SELECT a.proposalId, a.attachmentId, file.name, prop.projectId, prop.projectType, prop.customerId, " +
         "prop.resourceId, prop.auctionId, prop.periodId, auc.id, auc.comPeriodId, auc.aucPeriodId, " +
         "auc.type, proj.name, res.name, cust.name " +
         "FROM AttachPropPrimaryKey a " +
@@ -22,11 +23,19 @@ public class AttachmentFileService {
         "JOIN ProjectInfo proj ON prop.projectId = proj.id " +
         "JOIN ResourceInfo res ON prop.resourceId = res.id " +
         "JOIN Customer cust ON prop.customerId = cust.id " +
-        "WHERE (a.attachmentId) = (:attachment_id) AND (:file_name IS NULL OR LOWER(file.name) = LOWER(:file_name)) AND " +
-        "(:project_name IS NULL OR LOWER(proj.name) = LOWER(:project_name)) " +
-        "AND (:customer_name IS NULL OR LOWER(cust.name) = LOWER(:customer_name)) AND " +
-        "(:commitment_period_id IS NULL OR (auc.comPeriodId) = (:commitment_period_id)) " +
-        "AND (:resource_name IS NULL OR LOWER(res.name) = LOWER(:resource_name))").setParameter("attachment_id", attachment_id).setParameter("file_name", file_name).setParameter("project_name", project_name).setParameter("customer_name", customer_name).setParameter("commitment_period_id", commitment_period_id).setParameter("resource_name", resource_name).getResultList();
+        "WHERE (a.attachmentId) = (:attachment_id) AND " +
+        "(:file_name IS NULL OR (file.name) = (:file_name)) " +
+        "AND (:project_name IS NULL OR proj.name LIKE :project_name) " +
+        "AND (:customer_name IS NULL OR (cust.name) LIKE '%' || (:customer_name) || '%') " +
+        "AND (:commitment_period_id IS NULL OR auc.comPeriodId = :commitment_period_id) " +
+        "AND (:resource_name IS NULL OR (res.name) = (:resource_name))")
+        .setParameter("attachment_id", attachment_id)
+        .setParameter("file_name", file_name)
+        .setParameter("project_name", project_name)
+        .setParameter("customer_name", customer_name)
+        .setParameter("commitment_period_id", commitment_period_id)
+        .setParameter("resource_name", resource_name)
+        .getResultList();
         return var;
     }
 
--- src/main/java/com/example/demo/TestController.java
+++ src/main/java/com/example/demo/TestController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
     @GetMapping("/TestCall")
-    public List<AttachPropPrimaryKey> testCall() {
-        return AttachmentFileService.queryForDocuments(1001L, "Project docs.zip", "Gravity Works", "Lightening Power", 1L, "Big Generator");
+    public List<Object> testCall() {
+        return AttachmentFileService.queryForDocuments(1001L, "Project docs.zip", "Gravity Works", "Lightening Power ", 1L, "Big Generator");
     }
 }
