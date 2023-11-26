e9d9ef7410718f9a9d7475276e933f9979710ace
Aditya-Singh03 <95512205+Aditya-Singh03@users.noreply.github.com>
2023-11-26 01:19:14 -0500

    Created get request to specify specific parameters

--- src/main/java/com/example/demo/TestController.java
+++ src/main/java/com/example/demo/TestController.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,4 +12,17 @@ public class TestController {
     public List<Object> testCall() {
         return AttachmentFileService.queryForDocuments(1001L, "Project docs.zip", "Gravity Works", "Lightening Power ", 1L, "Big Generator");
     }
+
+    @GetMapping("/filterDocs")
+    public List<Object> filterDocs(@RequestParam(value = "attachment_id", required = false) Long attachment_id,
+            @RequestParam(value = "file_name", required = false) String file_name,
+            @RequestParam(value = "project_name", required = false) String project_name,
+            @RequestParam(value = "customer_name", required = false) String customer_name,
+            @RequestParam(value = "commitment_period_id", required = false) Long commitment_period_id,
+            @RequestParam(value = "resource_name", required = false) String resource_name) {
+        
+
+        return AttachmentFileService.queryForDocuments(attachment_id, file_name, project_name, customer_name, commitment_period_id, resource_name);
+    }
+
 }
