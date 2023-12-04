7d99b0a220619223cdbac691daad3d194a9c14e3
Ye Min Aung <redyaung2003@gmail.com>
2023-12-04 16:56:37 -0500

    Add data files and fix Base64 encoding

--- src/main/java/com/example/demo/DocumentDetailService.java
+++ src/main/java/com/example/demo/DocumentDetailService.java
@@ -1,6 +1,9 @@
 package com.example.demo;
 
 import java.util.Base64;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -10,13 +13,21 @@ import com.example.Model.metaReturn;
 
 @Component
 public class DocumentDetailService {
-    private String urlPrefix = "C:\\Users\\eting\\CS 320 File Store\\"; // for testing
+    private String prefix = "data/"; // for testing
 
-    public String encode(Long attachment_id, String filePath, String fileName) { // encode file's data in base64
-        String newFilePath = urlPrefix + filePath + "/" + fileName;
-        byte[] fileData = newFilePath.getBytes(); // turn file path String into byte[]
-        String base64EncodedFile = Base64.getEncoder().encodeToString(fileData); // encode in base64
-        return base64EncodedFile;
+    private String encodeFileToBase64(String filename) {
+        File file = new File(filename);
+        try {
+            byte[] fileContent = Files.readAllBytes(file.toPath());
+            return Base64.getEncoder().encodeToString(fileContent);
+        } catch (IOException e) {
+            throw new IllegalStateException("could not read file " + file, e);
+        }
+    }
+
+    public String encode(Long attachment_id, String filePath, String fileName) {
+        String newFilePath = prefix + filePath + "/" + fileName;
+        return encodeFileToBase64(newFilePath);
     }
 
     public metaReturn meta(String documentName, String projectName, String projectType, String periodStart, String periodEnd, String periodType, String customer, String resource, String resourceType, String auctionType, String proposalLabel, String attachmentType, float fileSize) { // return file's metaData information
