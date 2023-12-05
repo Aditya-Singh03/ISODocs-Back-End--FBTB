b266c1b5bb0a0d911cd4d127876d4a566bbbdf07
Ye Min Aung <redyaung2003@gmail.com>
2023-12-04 22:30:44 -0500

    Make EntityManager persistent and correct zip file path

--- src/main/java/com/example/demo/EntityService.java
+++ src/main/java/com/example/demo/EntityService.java
@@ -5,13 +5,22 @@ import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 
 public class EntityService {
-    EntityManagerFactory emf;
+    private static EntityManagerFactory emf = Persistence.createEntityManagerFactory("DATA");
+
+    private static EntityManager em = emf.createEntityManager();
 
     public static EntityManager getEntityManagerFactory() {
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("DATA");
-        return emf.createEntityManager();
+        return em;
     }
-
-    
     
 }
+
+// public class EntityService {
+//     private static EntityManagerFactory emf;
+
+//     public static EntityManager getEntityManagerFactory() {
+//         emf = Persistence.createEntityManagerFactory("DATA");
+//         return emf.createEntityManager();
+//     }
+    
+// }
