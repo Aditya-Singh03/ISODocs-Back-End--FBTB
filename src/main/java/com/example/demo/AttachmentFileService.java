d5685cffa3cd0796b0f4620e985fa0d7f4264b61
Aditya-Singh03 <95512205+Aditya-Singh03@users.noreply.github.com>
2023-12-11 23:42:40 -0500

    now also returns the total number of documents filtered

--- src/main/java/com/example/demo/AttachmentFileService.java
+++ src/main/java/com/example/demo/AttachmentFileService.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Service;
 
 
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.Query;
 import jakarta.persistence.TemporalType;
 
 @Service
@@ -137,6 +138,44 @@ public class AttachmentFileService {
         .setFirstResult((page - 1) * pageSize)
         .setMaxResults(pageSize)
         .getResultList();
+
+
+        String queryCount = "SELECT COUNT(*) " +
+        "FROM AttachPropPrimaryKey a " +
+        "JOIN Attachment file ON a.attachmentId = file.id " +
+        "JOIN ProposalInfo prop ON a.proposalId = prop.id " +
+        "JOIN AuctionInfo auc ON prop.auctionId = auc.id " +
+        "JOIN ProjectInfo proj ON prop.projectId = proj.id " +
+        "JOIN ResourceInfo res ON prop.resourceId = res.id " +
+        "JOIN Customer cust ON prop.customerId = cust.id " +
+        "JOIN period_info period_info ON auc.comPeriodId = period_info.id " +
+        "WHERE (:attachment_id IS NULL OR a.attachmentId = :attachment_id) AND " +
+        "(:file_name IS NULL OR LOWER(file.name) LIKE :file_name) " +
+        "AND (:project_name IS NULL OR LOWER(proj.name) LIKE :project_name) " +
+        "AND (:customer_name IS NULL OR LOWER(cust.name) LIKE :customer_name) " +
+        "AND (:commitment_period_id IS NULL OR auc.comPeriodId = :commitment_period_id) " +
+        "AND (:resource_name IS NULL OR LOWER(res.name) LIKE :resource_name) " +
+        "AND (CAST(:auc_begin_date AS DATE) IS NULL OR (auc.aucBeginDate >= :auc_begin_date AND auc.aucEndDate >= :auc_begin_date)) " +
+        "AND (CAST(:auc_end_date AS DATE) IS NULL OR (auc.aucBeginDate <= :auc_end_date AND auc.aucEndDate <= :auc_end_date)) " +
+        "AND (:proposal_label IS NULL OR LOWER(prop.proposalLabel) LIKE :proposal_label) " +
+        "AND (:auction_type IS NULL OR auc.type = :auction_type) ";
+
+        Query count = em.createQuery(queryCount)
+            .setParameter("attachment_id", attachment_id)
+            .setParameter("file_name", file_name == null ? null : file_name.toLowerCase())
+            .setParameter("project_name", project_name == null ? null : project_name.toLowerCase())
+            .setParameter("customer_name", customer_name == null ? null : customer_name.toLowerCase())
+            .setParameter("commitment_period_id", commitment_period_id)
+            .setParameter("resource_name", resource_name == null ? null : resource_name.toLowerCase())
+            .setParameter("auc_begin_date", aucBeginDate, TemporalType.DATE)
+            .setParameter("auc_end_date", aucEndDate, TemporalType.DATE)
+            .setParameter("proposal_label", proposal_label)
+            .setParameter("auction_type", auction_type);
+
+        Long total = (Long) count.getSingleResult();
+
+
+
         
         ArrayList<Map<String, Object>> jsonResults = new ArrayList<>();
 
@@ -169,6 +208,7 @@ public class AttachmentFileService {
                 jsonResult.put("periodType","" + columns[22].toString());
                 jsonResult.put("aucBeginDate","" + columns[23].toString());
                 jsonResult.put("aucEndDate","" + columns[24].toString());
+                jsonResult.put("itemCount", total);
 
                 
                 jsonResults.add(jsonResult);
